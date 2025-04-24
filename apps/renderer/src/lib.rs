e87b14c24af46d709d998f923839c11f5ed3c2bf
dominikj111 <dominik.jelinek111@gmail.com>
2025-04-24 07:18:30 +0100

    upgrade to bevy 0.14.2

--- apps/renderer/src/lib.rs
+++ apps/renderer/src/lib.rs
@@ -86,7 +86,7 @@ fn setup(
     commands.spawn(MaterialMesh2dBundle {
         mesh: meshes.add(Rectangle::default()).into(),
         transform: Transform::default().with_scale(Vec3::splat(128.)),
-        material: materials.add(Color::PURPLE),
+        material: materials.add(Color::from(bevy::color::palettes::basic::PURPLE)),
         ..default()
     });
 
--- apps/renderer/src/utils/color.rs
+++ apps/renderer/src/utils/color.rs
@@ -2,9 +2,9 @@ use bevy::prelude::Color;
 use rand::Rng;
 
 pub fn random_color() -> Color {
-    let mut rng = rand::thread_rng();
-    let r = rng.gen_range(0.0..=1.0);
-    let g = rng.gen_range(0.0..=1.0);
-    let b = rng.gen_range(0.0..=1.0);
-    Color::rgba_from_array([r, g, b, 1.])
+    let mut rng = rand::rng();
+    let r = rng.random_range(0.0..=1.0);
+    let g = rng.random_range(0.0..=1.0);
+    let b = rng.random_range(0.0..=1.0);
+    Color::linear_rgba(r, g, b, 1.)
 }
